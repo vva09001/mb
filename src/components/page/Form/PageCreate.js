cc843b97fe8109c2d728c0a7f068d5f8b77498c8
Hung Do <hung.do@miracles.vn>
2019-12-19 15:20:35 +0700

    [Page] Fix meta key page

--- src/components/page/Form/PageCreate.js
+++ src/components/page/Form/PageCreate.js
@@ -98,10 +98,15 @@ function PagesCreate({ onSubmit, handleChange, value, onDelete, deleteActive }) 
             </FormGroup>
             <FormGroup>
               <Label for="template">{t('page.template')}</Label>
-              <Input type="select" name="template" value={value.template} onChange={handleChange}>
-                <option>{t('Select')}</option>
-                <option>{t('page.default')}</option>
-                <option>{t('page.full')}</option>
+              <Input
+                type="select"
+                name="template"
+                value={value.template === undefined ? 1 : value.template}
+                onChange={handleChange}
+              >
+                <option value={1}>{t('Select')}</option>
+                <option value={2}>{t('page.default')}</option>
+                <option value={3}>{t('page.full')}</option>
               </Input>
             </FormGroup>
             <Button color="primary" type="submit">
@@ -119,15 +124,31 @@ function PagesCreate({ onSubmit, handleChange, value, onDelete, deleteActive }) 
             <h4>{t('seo')}</h4>
             <FormGroup>
               <Label for="exampleName">{t('meta.title')}</Label>
-              <Input type="text" name="meta_title" onChange={handleChange} />
+              <Input
+                type="text"
+                name="meta_title"
+                value={value.meta_title === undefined ? '' : value.name}
+                onChange={handleChange}
+              />
             </FormGroup>
             <FormGroup>
               <Label>{t('meta.keywords')}</Label>
-              <Input type="text" name="meta_keywords" onChange={handleChange} />
+              <Input
+                type="text"
+                name="meta_keywords"
+                value={value.meta_keywords === undefined ? '' : value.name}
+                onChange={handleChange}
+              />
             </FormGroup>
             <FormGroup>
               <Label for="exampleText">{t('meta.description')}</Label>
-              <Input type="textarea" name="meta_description" rows="5" onChange={handleChange} />
+              <Input
+                type="textarea"
+                name="meta_description"
+                value={value.meta_description === undefined ? '' : value.name}
+                rows="5"
+                onChange={handleChange}
+              />
             </FormGroup>
             <Button color="primary" type="submit">
               {t('save')}
